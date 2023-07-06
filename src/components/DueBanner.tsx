6fa0771a9a69276b5a2587e1ff1bb798c4b35ed9
omarknows <omarknows98@gmail.com>
2023-07-07 02:08:38 +0300

    changed banner design

--- src/components/DueBanner.tsx
+++ src/components/DueBanner.tsx
@@ -8,39 +8,14 @@ const DueBanner = () => {
   return (
     <div>
       {showBanner ? (
-        <div className="relative flex items-center gap-x-2 overflow-hidden bg-amber-400 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-          <div
-            className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-            aria-hidden="true"
-          >
-            <div
-              className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
-              style={{
-                clipPath:
-                  "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
-              }}
-            />
-          </div>
-          <div
-            className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-            aria-hidden="true"
-          >
-            <div
-              className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
-              style={{
-                clipPath:
-                  "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
-              }}
-            />
-          </div>
-          <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
-            <p className="text-md leading-6 text-gray-900">
-              You have tasks past due!
-            </p>
+        <div className="flex justify-between overflow-hidden bg-orange-600 w-[85vw] ml-[15vw] py-3">
+          <div></div>
+          <div className="flex items-center ml-2">
+            <p className="text-md text-gray-900">You have tasks past due!</p>
           </div>
           <button
             type="button"
-            className="bg-amber-400 rounded-md p-2 inline-flex items-center justify-center hover:text-gray-500 hover:bg-amber-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-amber-600"
+            className="bg-orange-600 rounded-md p-2 inline-flex items-center justify-center hover:text-gray-500 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-700 mr-2"
             onClick={closeBanner}
           >
             <span className="sr-only">Close menu</span>
